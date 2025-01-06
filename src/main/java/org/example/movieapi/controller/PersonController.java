d921c73f14b79d5d630d0d5e3e03ce6f3071661e
matthcol <matthias.colin@gmail.com>
2025-01-06 11:49:09 +0100

    add validation

--- src/main/java/org/example/movieapi/controller/PersonController.java
+++ src/main/java/org/example/movieapi/controller/PersonController.java
@@ -7,11 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 
 @RestController
@@ -23,7 +22,7 @@ public class PersonController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public PersonDtoSimple add(@RequestBody PersonDtoCreate person){
+    public PersonDtoSimple add(@RequestBody @Valid PersonDtoCreate person){
         return personService.add(person);
     }
 
