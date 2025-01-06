b9defbb4bc80e561d91ab63ad232bac9afa36120
matthcol <matthias.colin@gmail.com>
2025-01-06 11:40:57 +0100

    impl add person

--- src/main/java/org/example/movieapi/controller/PersonController.java
+++ src/main/java/org/example/movieapi/controller/PersonController.java
@@ -2,8 +2,13 @@ package org.example.movieapi.controller;
 
 import org.example.movieapi.dto.PersonDtoCreate;
 import org.example.movieapi.dto.PersonDtoSimple;
+import org.example.movieapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -13,9 +18,13 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/person")
 public class PersonController {
 
+    @Autowired
+    private PersonService personService;
+
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public PersonDtoSimple add(@RequestBody PersonDtoCreate person){
-        return null;
+        return personService.add(person);
     }
 
 }
